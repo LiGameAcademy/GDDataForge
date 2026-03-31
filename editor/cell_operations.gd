4f828073e1a319970d136fa2ce83e2906ddf6ac3
OldLee <2410250284@qq.com>
2026-03-31 11:18:49 +0800

    feat(editor): 添加单元格操作函数
    - add_row: 添加行
    - delete_selected_row: 删除选中行
    - add_column: 添加列
    - update_cell: 更新单元格
    - _validate_and_convert_value: 类型校验转换

---
+++ editor/cell_operations.gd
@@ -0,0 +1,87 @@
+## 添加行操作
+
+func add_row(row_id: String, row_data: Dictionary) -> void:
+	if row_id.is_empty():
+		_update_status("ID 不能为空")
+		return
+	
+	if _current_table.has(row_id):
+		_update_status("ID 已存在: " + row_id)
+		return
+	
+	_current_table[row_id] = row_data
+	_refresh_table_view()
+	_update_status("已添加行: " + row_id)
+	table_modified.emit(_current_table_name)
+
+## 删除选中行
+func delete_selected_row() -> void:
+	var item = _table_view.get_selected()
+	if not item:
+		_update_status("请先选择一行")
+		return
+	
+	var row_id = _table_view.get_item_text(item, 0)
+	_current_table.erase(row_id)
+	_refresh_table_view()
+	_update_status("已删除行: " + row_id)
+	table_modified.emit(_current_table_name)
+
+## 添加列
+func add_column(col_name: String, col_type: String = "string") -> void:
+	if col_name.is_empty():
+		_update_status("列名不能为空")
+		return
+	
+	if col_name in _current_table_columns:
+		_update_status("列已存在: " + col_name)
+		return
+	
+	_current_table_columns.append(col_name)
+	_current_table_types.append(col_type)
+	_refresh_table_view()
+	_update_status("已添加列: " + col_name)
+	table_modified.emit(_current_table_name)
+
+## 删除选中列
+func delete_selected_column() -> void:
+	# TODO: 实现列删除
+	_update_status("列删除功能开发中...")
+
+## 更新单元格
+func update_cell(row_id: String, col_name: String, value: Variant) -> void:
+	if not _current_table.has(row_id):
+		_update_status("行不存在: " + row_id)
+		return
+	
+	# 类型检查
+	var col_index = _current_table_columns.find(col_name)
+	if col_index >= 0:
+		var expected_type = _current_table_types[col_index]
+		value = _validate_and_convert_value(value, expected_type)
+	
+	_current_table[row_id][col_name] = value
+	table_modified.emit(_current_table_name)
+
+## 类型校验和转换
+func _validate_and_convert_value(value: Variant, expected_type: String) -> Variant:
+	match expected_type:
+		"int":
+			if typeof(value) == TYPE_STRING:
+				return value.to_int()
+			return int(value)
+		"float":
+			if typeof(value) == TYPE_STRING:
+				return value.to_float()
+			return float(value)
+		"bool":
+			if typeof(value) == TYPE_STRING:
+				return bool(value.to_int())
+			return bool(value)
+		"vector2":
+			if typeof(value) == TYPE_STRING:
+				var parts = value.split(",")
+				if parts.size() >= 2:
+					return Vector2(parts[0].to_float(), parts[1].to_float())
+			return value
+	return value
